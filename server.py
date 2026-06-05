fde51d37f6f3f70b292b59a1bb1453cb3e272cb7
Pucho <martinezochandarenam@gmail.com>
2026-06-05 21:21:00 +0200

    feat: split DebugMiddleware from server file

--- server.py
+++ server.py
@@ -1,10 +1,7 @@
 import asyncio
 import subprocess
 from fastmcp import Context
-from fastmcp.server.middleware import (
-    Middleware,
-    MiddlewareContext
-)
+
 
 from pathlib import Path
 import httpx
@@ -14,48 +11,13 @@ import sys
 from core.mcp_instance import mcp
 from core.logger import logger
 from services.notes_service import NotesService
-
-class DebugMiddleware(Middleware):
-
-    async def on_call_tool(
-        self,
-        context: MiddlewareContext, # me da acceso a toda la request   method/source/timestamp/type/message...
-        call_next
-    ):
-        tool_name = getattr(context.message, "name", "<sin nombre>")
-        
-        before_message = f"ANTES -> method={context.method} tool={tool_name}"
-        logger.info(before_message)
-
-        # print(
-        #     "ANTES",
-        #     file=sys.stderr
-        # )
-
-        # print(
-        #     f"→ {context.method}",
-        #     file=sys.stderr
-        # )
-
-        result = await call_next(context) # Permite que la peticion fluya que no se corte en este midelware
-
-        # print(
-        #     f"→ {context.method}",
-        #     file=sys.stderr
-        # )
-
-        # print(
-        #     f"← {context.method}",
-        #     file=sys.stderr
-        # )
-
-        after_message = f"DESPUES -> method={context.method} tool={tool_name}"
-        logger.info(after_message)
-
-        return result
+from middleware.debug import DebugMiddleware
 
 
-mcp.add_middleware(DebugMiddleware())
+
+mcp.add_middleware(
+    DebugMiddleware()
+)
 
 # -----PROMPTS ------
 
