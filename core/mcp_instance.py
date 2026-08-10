4f17656b85b5e9b2c4c35dc622c1177a1a5e7d84
Pucho <martinezochandarenam@gmail.com>
2026-08-10 10:38:02 +0200

    feat: add auth check from fatsMcp

--- core/mcp_instance.py
+++ core/mcp_instance.py
@@ -1,7 +1,14 @@
 from fastmcp import FastMCP
 from core.lifespan import app_lifespan
+from core.auth import create_auth_provider
+
+from config.settings import load_settings
+
+
+settings = load_settings()
 
 mcp = FastMCP(
     "Filesystem-server",
-    lifespan=app_lifespan
+    lifespan=app_lifespan,
+    auth=create_auth_provider(settings),
 )
