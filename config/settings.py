c24b1ccbd04d1107d5586d818db8f33250343ba0
Pucho <martinezochandarenam@gmail.com>
2026-08-10 10:39:41 +0200

    feat: change settings for new provider

--- config/settings.py
+++ config/settings.py
@@ -16,10 +16,15 @@ class Settings:
     LANGFUSE_SECRET_KEY: str = "change-me"
     LANGFUSE_PUBLIC_KEY: str = "change-me"
     LANGFUSE_BASE_URL: str = "https://cloud.langfuse.com"
-    auth_audience: str = "your-audience"
-    auth_issuer: str = "https://your-issuer.com/"
-    # jwt_options = { "verify_signature": True}
-    auth_jwks_url: str = "https://your-issuer.com/.well-known/jwks.json"  # URL to fetch JWKS for token verification
+
+    # auth_config_url: str = ""
+    # auth_client_id: str = ""
+    # auth_client_secret: str = ""
+
+    jwks_uri: str = ""
+    auth_issuer: str = ""
+    auth_base_url: str = "http://localhost:8000/mcp"
+    auth_audience: str = "https://your-issuer.com/api"
 
 def load_settings() -> Settings:
     """Load settings from the .env file."""
@@ -31,13 +36,13 @@ def load_settings() -> Settings:
         transport=os.getenv("TRANSPORT", Settings.transport),
         api_key=os.getenv("MCP_API_KEY", Settings.api_key),
         db_path=os.getenv("DB_PATH", Settings.db_path),
+
         LANGFUSE_SECRET_KEY=os.getenv("LANGFUSE_SECRET_KEY", Settings.LANGFUSE_SECRET_KEY),
         LANGFUSE_PUBLIC_KEY=os.getenv("LANGFUSE_PUBLIC_KEY", Settings.LANGFUSE_PUBLIC_KEY),
         LANGFUSE_BASE_URL=os.getenv("LANGFUSE_BASE_URL", Settings.LANGFUSE_BASE_URL),
-        auth_audience=os.getenv("AUTH_AUDIENCE", Settings.auth_audience),
+
+        jwks_uri=os.getenv("AUTH_JWKS_URI", Settings.jwks_uri),
         auth_issuer=os.getenv("AUTH_ISSUER", Settings.auth_issuer),
-        auth_jwks_url=os.getenv("AUTH_JWKS_URL", Settings.auth_jwks_url),
-        # jwt_options={
-        #     "verify_signature": os.getenv("JWT_VERIFY_SIGNATURE", "True").lower() in ("true", "1", "t")
-        # }
+        auth_base_url=os.getenv("AUTH_BASE_URL", Settings.auth_base_url),
+        auth_audience=os.getenv("AUTH_AUDIENCE", Settings.auth_audience)
     )
